935b558d0a0f5dbe6b1650d13e82bda4a51fc983
Carolina Valladares <carolinalimacvalladares@gmail.com>
2023-11-24 20:54:44 -0300

    add reference comments

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -4,10 +4,14 @@ import { index, register, show } from "../controllers/userController";
 const route = Router();
 
 // routes
+
+// cadastrar usuário
 route.post("/register", register);
 
+// listar todos os usuários
 route.get("/list", index);
 
+// mostrar um usuário
 route.get("/:id", show);
 
 export default route;
